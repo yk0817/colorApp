c1d15fd636e9cae4a85975012cebf0eea7dd3cb3
山本　憲 <em072029@gmail.com>
2018-08-26 16:23:58 +0900

    colorを表示する。

--- colorQuizApp/ViewController.swift
+++ colorQuizApp/ViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var colorLabel: UILabel!
+    
+    var colorR = 0
+    var colorG = 0
+    var colorB = 0
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        colorR = Int(arc4random() % 256)
+        colorG = Int(arc4random() % 256)
+        colorB = Int(arc4random() % 256)
+        colorLabel.text = "R=\(colorR), G=\(colorG), B=\(colorB)"
+    }
 
+    @IBAction func returnTop(segue: UIStoryboardSegue) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ colorQuizApp/colorViewController.swift
@@ -0,0 +1,34 @@
+//
+//  colorViewController.swift
+//  colorQuizApp
+//
+//  Created by 山本　憲 on 2018/08/26.
+//  Copyright © 2018年 山本　憲. All rights reserved.
+//
+
+import UIKit
+
+class colorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
